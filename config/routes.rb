401b39bc86a369f0a74df3b5b24683363b97f78e
arnoldmanzano <arnold.manzano@gmail.com>
2016-03-02 14:24:12 +0000

    Implemented omniauth and facebook sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+
+  devise_for :users,
+    :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
+
+  # devise_scope :user do
+  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
 
   resources :restaurants do
     resources :reviews
